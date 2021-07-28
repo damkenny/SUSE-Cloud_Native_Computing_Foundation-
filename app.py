40a45ee496bad571da24b798d299716797667a3e
Kehinde <adeniyekehinde0@gmail.com>
2021-07-28 10:56:23 +0000

    updating dockerfile

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from flask import Flask
+from flask import json
+
 app = Flask(__name__)
-@app.route('/')
-def welcome():
-    return "this is my first Flask app! Yay"
-if __name__ == '__main__':
- app.run()
+
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
